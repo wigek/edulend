319375b1bb4b2f55ad1c86d5198556ec38648903
andres0818 <c.andres.perez.h@gmail.com>
2025-05-03 14:59:40 -0600

    se agrega formulario recuperar contrasenia

--- js/login.js
+++ js/login.js
@@ -4,20 +4,24 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function Init(){
-    const actionModal = document.querySelectorAll("a");
-
     const fondoFormulario = document.getElementById("registrarse-iniciar-sesion")
+    const actionModal = document.querySelectorAll("a");
 
     actionModal.forEach(e => {
         e.addEventListener("click", e => {
-
-            console.log(typeof(e.target.dataset.registrarse))
 
             if(e.target.dataset.registrarse == "true"){
                 fondoFormulario.style.transform = 'translateX(-51%)';
             }
             else{
                 fondoFormulario.style.transform = 'translateX(51%)';
+            }
+
+            if(e.target.dataset.show){
+                document.querySelector(`#${e.target.dataset.show}`).showModal()
+            }
+            else if(e.target.dataset.hidde){
+                document.querySelector(`#${e.target.dataset.hidde}`).close();
             }
         })
     });
