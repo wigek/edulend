06e3e15b97b673e5fbb9dc1b019d4a3ef94fb6dd
andres0818 <c.andres.perez.h@gmail.com>
2025-05-17 15:02:20 -0600

    Se agrega funciones a los botones

--- js/lend_state.js
+++ js/lend_state.js
@@ -1,13 +1,34 @@
-document.getElementById("formularioEntrega").addEventListener("submit", function(event) {
-      event.preventDefault(); // evitar que se cierre automáticamente
-      const fecha = document.getElementById("fechaEntrega").value;
-      alert("Fecha de entrega ingresada: " + fecha);
-      document.getElementById("msjEntrega").close();
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("formularioEntrega").addEventListener("submit", function(event) {
+    event.preventDefault(); // evitar que se cierre automáticamente
 
-document.getElementById("formularioProrroga").addEventListener("submit", function(event) {
-      event.preventDefault(); // evitar que se cierre automáticamente
-      const fecha = document.getElementById("fechaProrroga").value;
-      alert("Fecha de prorroga ingresada: " + fecha);
-      document.getElementById("msjProrroga").close();
-    });
+    const fecha = document.getElementById("fechaEntrega").value;
+    let mensaejeFecha = "Fecha de entrega ingresada: " + fecha;
+
+    document.getElementById("msjEntrega").close();
+    enviarFormulario(event, "Entrega realizada", mensaejeFecha)
+  });
+
+  document.getElementById("formularioProrroga").addEventListener("submit", function(event) {
+    event.preventDefault(); // evitar que se cierre automáticamente
+
+    const fecha = document.getElementById("fechaProrroga").value;
+    let mensaejeFecha = "Fecha de prorroga ingresada: " + fecha
+
+    document.getElementById("msjProrroga").close();
+    enviarFormulario(event, "Prorroga realizada", mensaejeFecha)
+  });
+
+
+  function enviarFormulario(e, mensaje, mensaejeFecha) {
+      Swal.fire({
+          title: mensaje,
+          text: mensaejeFecha,
+          icon: "success",
+          draggable: true
+      });
+
+      e.target.reset();
+  }
+
+});
